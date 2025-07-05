1dc95b70441fb2cb08006ed7c1c01f47ce2ee31a
Alex <lx2006861@gmail.com>
2025-07-05 19:10:22 +0300

    themeProvider change type any

--- src/app/providers/themeProviders/ui/ThemeProvider.tsx
+++ src/app/providers/themeProviders/ui/ThemeProvider.tsx
@@ -1,10 +1,13 @@
-import { FC, useMemo, useState } from "react";
+import { FC, ReactNode, useMemo, useState } from "react";
 import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "../lib/themeContext";
 
+interface ThemeProviderProps {
+	children?: ReactNode;
+}
 
 const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
 
-export const ThemeProvider: FC<any> = ({ children }) => {
+export const ThemeProvider: FC<ThemeProviderProps> = ({ children }) => {
 	const [theme, setTheme] = useState<Theme>(defaultTheme);
 
 	const defaultProps = useMemo(() => (
